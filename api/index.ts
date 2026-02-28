ca5283e52b0281f5484fe763307bfb3f2bcd6428
Jean Marc Bassahak <bjm.graphicdesigner@gmail.com>
2026-02-28 17:12:38 +0100

    Fix: build errors for vercel

--- api/index.ts
+++ api/index.ts
@@ -3,8 +3,9 @@ import { AppModule } from '../src/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
+import { Express } from 'express';
 
-const server = express();
+const server: Express = express();
 
 export const createNestServer = async (expressInstance: any) => {
   const app = await NestFactory.create(
